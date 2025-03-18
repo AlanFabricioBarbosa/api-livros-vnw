999d67a5cdc981a74928a40502b6330fcb0d81e0
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-03-18 15:38:44 -0300

    feat: Feito API com métodos para adicionar um livro, excluir um livro e listar todos os livros da aplicação

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify
+from marshmallow import Schema, fields, ValidationError
+import sqlite3
+
+app = Flask(__name__)
+
+def init_db():
+   with sqlite3.connect('database.db') as conn:
+      conn.execute(
+            """CREATE TABLE IF NOT EXISTS livros(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               titulo TEXT NOT NULL,
+               categoria TEXT NOT NULL,
+               autor TEXT NOT NULL,
+               imagem_url TEXT NOT NULL
+            )"""
+      )
+      print('Banco de dados criado!')
+
+init_db()
+class LivroSchema(Schema):
+   id = fields.Int(dump_only=True)
+   titulo = fields.Str(required=True)
+   categoria = fields.Str(required=True)
+   autor = fields.Str(required=True)
+   imagem_url = fields.Str(required=True)
+
+@app.route('/')
+def home_page():
+   return '<h2>Flask Home Page</h2>'
+
+@app.route('/doar', methods=['POST'])
+def doar():
+   dados = request.get_json()
+   livro_schema = LivroSchema()
+
+   try:
+      livro = livro_schema.load(dados)
+   except ValidationError as err:
+      return jsonify({"erro": err.messages}), 400
+
+   with sqlite3.connect('database.db') as conn:
+      conn.execute(""" 
+            INSERT INTO livros (titulo, categoria, autor, imagem_url)
+            VALUES (?, ?, ?, ?)""",
+                     (livro['titulo'], livro['categoria'], livro['autor'], livro['imagem_url']))
+      conn.commit()
+      print(f"Livro inserido: {livro}")
+
+   return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
+
+@app.route('/livros', methods=['GET'])
+def listar_livros():
+   with sqlite3.connect('database.db') as conn:
+      livros = conn.execute("SELECT * FROM livros").fetchall()
+
+      livros_formatados = []
+      for livro in livros:
+            livro_dict = {
+               "id": livro[0],
+               "titulo": livro[1],
+               "categoria": livro[2],
+               "autor": livro[3],
+               "imagem_url": livro[4]
+            }
+            livros_formatados.append(livro_dict)
+
+      livro_schema = LivroSchema(many=True)
+      livros_serializados = livro_schema.dump(livros_formatados)
+
+   return jsonify(livros_serializados)
+
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+   with sqlite3.connect('database.db') as conn:
+      cur = conn.cursor()
+      cur.execute("SELECT * FROM livros WHERE id=?", (id,))
+      livro = cur.fetchone()
+
+      if livro is None:
+            return jsonify({"erro": "Livro não encontrado"}), 404
+
+      conn.execute("DELETE FROM livros WHERE id=?", (id,))
+      conn.commit()
+
+   return jsonify({"mensagem": f"Livro com id {id} excluído com sucesso"}), 200
+
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
